bf752002ec359ce8f9b5981fdb8d70af28f370df
Moskate <47504837+Moskate@users.noreply.github.com>
2019-09-30 20:34:38 -0700

    new version

--- MainUi.py
+++ MainUi.py
@@ -8,6 +8,7 @@ class SampleApp(tk.Tk):
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.title("Event Planner")
+
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=5)
@@ -31,34 +32,51 @@ class SampleApp(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
+    def refresh(self):
+        self.weight_entry.delete(0,"end")
+        self.text.delete("1.0","end")
+
+
+
+    def close(self):
+        self.destroy()
 
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.configure(background="orange")
         label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10, padx = 10)
+        label.pack(side="top", fill="x", pady=100, padx = 10)
 
-        button1 = tk.Button(self, text="Go to Page One", command=lambda: controller.show_frame("EventPage"))
-        button2 = tk.Button(self, text="Go to Page Two", command=lambda: controller.show_frame("AccountPage"))
+        button1 = tk.Button(self, text="Go to Event Page", command=lambda: controller.show_frame("EventPage"))
+        button2 = tk.Button(self, text="Go to Account Page", command=lambda: controller.show_frame("AccountPage"))
         button1.pack()
         button2.pack()
         textentry = tk.Text(self, width=20, height=1, wrap=tk.WORD, background="white")
         textentry.pack(side =tk.TOP)
-
 
 class EventPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.config(background = "blue")
         label = tk.Label(self, text="EventPage", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=100, padx = 100)
+        label.pack(side="left", fill="x", pady=10, padx = 10)
         button = tk.Button(self, text="Go to the start page", command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        button.pack(side = "left")
+        outputBox = tk.Text(self , width= 10, height= 10, wrap= tk.WORD )
+        outputBox.pack()
+        Button2 = tk.Button(self, text = "testing", command = lambda :self.click(outputBox, "testing the search"))
+        Button2.pack()
+        ExitButton= tk.Button(self, text="close window", command= lambda: controller.close())
+        ExitButton.pack(side= "right",pady=10, padx = 10)
 
-
+    def click(self, outputbox, searchtest):
+        outputbox.delete(1.0,tk.END)
+        outputbox.insert(tk.END,searchtest)
 class AccountPage(tk.Frame):
 
     def __init__(self, parent, controller):
