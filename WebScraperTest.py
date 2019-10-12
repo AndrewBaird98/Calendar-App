95df17d58bd1fecdfc2bb7d0815873b4e09effd9
Moskate <47504837+Moskate@users.noreply.github.com>
2019-10-12 11:22:20 -0700

    New
    Should be easier to run

--- WebScraperTest.py
+++ WebScraperTest.py
@@ -2,16 +2,16 @@
 from selenium import webdriver
 from  selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import os,sys,inspect
 
-#result = requests.get("https://www.google.com/search?rlz=1C1CHBD_enUS847US847&biw=958&bih=959&ei=062eXb3zLrKv0PEPrsOU-AM&q=concerts&oq=concerts&gs_l=psy-ab.3..0l10.32579.35048..35459...0.1..0.142.806.8j1......0....1..gws-wiz.....6..0i71j0i362i308i154i357j0i273j0i131j0i67.h-o3ZcasFBs&uact=5&ibp=htl;events&rciv=evn#fpstate=tldetail&htidocid=_-SGMC1iCVoU4VvkJpA8-A%3D%3D&htivrt=events")
+current_folder= os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe() ))[0]))
+chromedriver = os.path.join(current_folder,"chromedriver.exe")
 
-import time
-chromedriver= "/workspace/untitled1/driver/chromedriver"
 options = Options()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
-driver = webdriver.Chrome(chromedriver, options=options)
+driver = webdriver.Chrome(executable_path=chromedriver, options=options)
 driver.get("https://www.google.com/")
 soup = BeautifulSoup(driver.page_source,'lxml')
 for links in soup.find_all('a'):
