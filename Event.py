a604b881f8d5e8c771b88de865e26570f0ae294a
AndrewBaird98 <54638347+AndrewBaird98@users.noreply.github.com>
2019-11-04 14:14:13 -0800

    Updated to include recurrence, attendees, and to make things such as time more friendly for google calendar

--- Event.py
+++ Event.py
@@ -1,10 +1,11 @@
 class Event:
-
+   
 #initiates event attributes
-    def __init__(self, name, date, time, city, state, price, address, category, description, link, restriction):
+    def __init__(self, name, date, startTime, endTime, city, state, price, address, category, description, link, restriction, recurrence, attendees):
         self.name = name
         self.date = date
-        self.time = time
+        self.startTime = startTime
+        self.endTime = endTime
         self.city = city
         self.state = state
         self.price = price
@@ -13,12 +14,15 @@ class Event:
         self.description = description
         self.link = link
         self.restriction = restriction
+        self.recurrence = recurrence
+        self.attendees = attendees
 
 #this function used as an example to display event attrubites
     def displayExample(self):
         print(a.name)
         print(a.date)
-        print(a.time)
+        print(a.startTime)
+        print(a.endTime)
         print(a.city)
         print(a.state)
         print(a.price)
@@ -27,13 +31,14 @@ class Event:
         print(a.description)
         print(a.link)
         print(a.restriction)
+        print(a.recurrence)
+        print(a.attendees)
 
 
 
-
-
+attendees = ['baird013@cougars.csusm.edu']
 #initilize event attributes
-a = Event("Taco Tuesday", "10 Oct 2019", "8:00 PM", "San Marcos", "CA", "$6", "300 San Marcos Blvd", "Nightlife", "Enjoy Tacos half price", "www.google.com", "+21")
+a = Event("Taco Tuesday", "2019-11-28", "09:00:00-07:00", "17:00:00-07:00", "San Marcos", "CA", "$6", "300 San Marcos Blvd", "Nightlife", "Enjoy Tacos half price", "www.google.com", "+21", 'RRULE:FREQ=DAILY;COUNT=2;', attendees)
 
 #display them
 a.displayExample()
