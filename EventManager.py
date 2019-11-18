5c4ce1f0c7c4515ac5706013032c21d8f59015f5
Moskate <47504837+Moskate@users.noreply.github.com>
2019-11-17 20:16:46 -0800

    Some changes

--- EventManager.py
+++ EventManager.py
@@ -13,31 +13,29 @@ SortedEventList = []
 
 
 def FindEvents():
-    current_folder= os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-    chromedriver = os.path.join(current_folder,"chromedriver")
+    if len(FullEventList) == 0:
+        current_folder= os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+        chromedriver = os.path.join(current_folder,"chromedriver")
 
-    options = Options()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
-    driver.get("https://www.sandiego.org/explore/events.aspx")
-    driver.find_element_by_xpath('//button[@class="submit-button__secondary load-more"]').click()
-    time.sleep(0.1)
-    soup = BeautifulSoup(driver.page_source,'lxml')
-    #print(soup)
-    eventlist = []
-    for links in soup.find_all('section',class_="result"):
-        #print(links.find('div', class_="result__tag").get_text())
-        E = Event(links.find('a',class_="result__title-link").get_text(),links.find('div',class_="result__dates").get_text(),
+        options = Options()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(executable_path=chromedriver, options=options)
+        driver.get("https://www.sandiego.org/explore/events.aspx")
+        driver.find_element_by_xpath('//button[@class="submit-button__secondary load-more"]').click()
+        time.sleep(0.1)
+        soup = BeautifulSoup(driver.page_source,'lxml')
+        #print(soup)
+        eventlist = []
+        for links in soup.find_all('section',class_="result"):
+            #print(links.find('div', class_="result__tag").get_text())
+            E = Event(links.find('a',class_="result__title-link").get_text(),links.find('div',class_="result__dates").get_text(),
                   "Not Know","Not Known","Not Known","Not Known","Not Known", links.find('div',class_="result__tag").get_text(),
                   "Not Know",links.find('a',class_="result__cta-link").get('href'),"Not Known", "Not Known", "Not Known", "Not Known")
-        FullEventList.append(E)
+            FullEventList.append(E)
 
-        #E.displayExample()
-
-
-    driver.close()
+        driver.close()
 def EventInfoDisplay(EInfo,OutPutBox):
     current_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
     chromedriver = os.path.join(current_folder, "chromedriver")
@@ -61,11 +59,16 @@ def EventInfoDisplay(EInfo,OutPutBox):
             OutPutBox.append(found.text)
     except NoSuchElementException:
         pass
-def DisplayFullTheList():
-    for e in FullEventList:
+    driver.close()
+# to be displayed in the UI That way we have one main display
+def DisplayTheList(List):
+    for e in List:
+        #To pe printed on to the UI
         e.displayExample()
-
 def SortTheList(Sortby):
-    # to sort the list depeding on what is pasted to it
+    print("hello")
 
 
+if __name__ == '__main__':
+    # so it does not run at import and wait to use the functions
+    print('should not run')
